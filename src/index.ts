db04ff69b61a92bfd92b781db1e94bff99fcf727
Adam Scott <iamadamone@gmail.com>
2025-04-17 10:39:40 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+import './config'; // Load environment variables
---
+++ src/types/utils.d.ts
@@ -0,0 +1,7 @@
+declare module 'express-async-errors';
+
+type DatabaseConstraintError = {
+  type: 'unique' | 'check' | 'not null' | 'foreign key' | 'unknown';
+  columnName?: string;
+  message?: string;
+};
